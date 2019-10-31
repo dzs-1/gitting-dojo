ccc14bfd959c53f0428aff2c56ff97852c8afa6e
Tóth Máté <tothmate911@gmail.com>
2019-10-31 13:11:42 +0100

    add shout

--- gittie.py
+++ gittie.py
@@ -2,15 +2,15 @@ def introduce():
     print("Hello, I'm Toth Mate!")
     print("Hello, I'm Peter!")
 
-
 def add(a, b):
     sum = a + b
     return sum
 
-    
 def joke():
     print("this should be a big joke!")
 
-
 def go():
     return "going"
+
+def shout():
+    print("SHOUT!!!")
