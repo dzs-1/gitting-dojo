12cf557a81fdbcd999133c27f14ed19c331f68b1
Tóth Máté <tothmate911@gmail.com>
2019-10-31 11:35:09 +0100

    add joke

--- gittie.py
+++ gittie.py
@@ -1,12 +1,5 @@
 def introduce():
     print("Hello, I'm Gittie!")
 
-<<<<<<< HEAD
-
-def add(a, b):
-    sum = a + b
-    return sum
-=======
-def add(a, b):
-    return a + b
->>>>>>> dbfe5775fddf9bb65c470ee5c7e39ccca09029d6
+def joke():
+    print("a joke!")
